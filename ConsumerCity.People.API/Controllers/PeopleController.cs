236f0e6c33dbac7cf2a4d85c60afd2cebf74d5d2
Pedro Azevedo <pedro.azvm@gmail.com>
2021-10-05 16:44:22 -0300

    Update PeopleController.cs

--- ConsumerCity.People.API/Controllers/PeopleController.cs
+++ ConsumerCity.People.API/Controllers/PeopleController.cs
@@ -38,7 +38,7 @@ namespace ConsumerCity.People.API.Controllers
         [HttpGet("v")]
         public string GetVersion()
         {
-            return "5";
+            return "6";
         }
     }
 }
